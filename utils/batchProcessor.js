a6383c54dd1a35faaf27b77eaa500c5ca4d36493
Anurag Sonkar <anuragsonkar053@gmail.com>
2025-08-28 17:27:33 -0700

    pc-c

--- utils/batchProcessor.js
+++ utils/batchProcessor.js
@@ -8,9 +8,9 @@ const ProcessingLog = require('../models/processingLogSchema');
 // Configuration
 const API_URL = `${base_url}/api/deeds/processDeeds`;
 const STATS_URL = `${base_url}/api/deeds/processingStats`;
-const BATCH_SIZE = 10;
-const MAX_BATCHES = 100;
-const DELAY_MS = 2000;
+const BATCH_SIZE = 2;
+const MAX_BATCHES = 2;
+const DELAY_MS = 1000;
 
 // Log file setup
 const logFile = path.join(__dirname, 'automation-log.txt');
